c6a91536440a60f22ba4e76bb7a274ef345c4d65
orhann-berk <orhan.berk.durak@motopp.nl>
2026-02-24 17:11:48 +0100

    add user, delete user, get users operations added.

--- database.py
+++ database.py
@@ -1,15 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from models import Base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./Social-Media-FastAPI.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./social.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
 
 def get_db():
     # open the db session
@@ -20,3 +19,5 @@ def get_db():
     finally:
         # close the db
         db.close()
+
+Base.metadata.create_all(bind=engine)
