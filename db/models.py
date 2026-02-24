34733b3ded6297bff95437e049f9cb12e4d6c136
orhann-berk <orhan.berk.durak@motopp.nl>
2026-02-24 03:06:17 +0100

    try to do login and update operations but receiving errors

--- db/models.py
+++ db/models.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
 # model is the type of data that goes into the db
 class UserAuthModel(BaseModel):
+    user_id: int
     username: str
     password: str
     email: str
+    is_logged_in: bool
 class LoginUser(BaseModel):
     username: str
     password: str
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
-from db.models import UserAuthModel, LoginUser
+from db.models import UserAuthModel
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
@@ -18,14 +19,22 @@ async def register(user_id: int, user: UserAuthModel):
             raise HTTPException(status_code=409, detail="Email already registered")
     # store the user
     new_user = {"user_id": len(users), "username": user.username, "email": user.email, "password": user.password}
+    UserAuthModel.user_id = len(users)
     users.append(new_user)
     return {"user_id": user_id, "user": user}
 
 @app.get("/login")
-async def login(username: str, password: str):
+async def login_user(username: UserAuthModel.username, password: UserAuthModel.password):
     for user in users:
         if user["username"] == username and user["password"] == password:
-            return {"message": "congratulations you successfully logged in"}
+            UserAuthModel.is_logged_in = True
+            return {"message": "Login successful"}
         else:
             raise HTTPException(status_code=404, detail="Incorrect username or password")
     return None
+
+@app.put("/update/{user_id}")
+async def update_user(user_id: UserAuthModel.user_id, user: UserAuthModel):
+    update_user_encoded = jsonable_encoder(user)
+    users[user_id] = update_user_encoded
+    return update_user_encoded
